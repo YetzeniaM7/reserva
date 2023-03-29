e990e10c850f76875b01855e7442b1d435018c25
OchoittaDev <87886442+OchoittaDev@users.noreply.github.com>
2023-03-29 09:48:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tRouter = require('./routes/trabajo');
 var ERouter = require('./routes/equipo');
+var personalrouter = require('./routes/personal');
+var solicitanterouter = require('./routes/solicitante');
 var app = express();
 
 // view engine setup
@@ -27,6 +29,9 @@ app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/trabajo', tRouter);
 app.use('/equipo', ERouter);
+app.use('/personal', personalrouter);
+app.use('/solicitante', solicitanterouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
