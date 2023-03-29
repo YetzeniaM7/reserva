07ec012439496cc65495e75820a781aa021789df
OchoittaDev <87886442+OchoittaDev@users.noreply.github.com>
2023-03-29 12:58:38 -0400

    Update solicitantem.js

--- models/solicitantem.js
+++ models/solicitantem.js
@@ -26,7 +26,7 @@ class solicitantemodel {
     }
     //Actualizar-Editar
     Actualizar(id, ns){
-        return new Promise ((resolve, reject)=>{//'UPDATE `espacio` SET `nombre`= ?,`descripcion`= ?,`direccion`= ?,`estatus`= ? WHERE id = ? 
+        return new Promise ((resolve, reject)=>{
             var query = connection.query('UPDATE `solicitante` SET `nombre_apellido`= ?,`fecha_nacimiento`= ?,`direccion`= ?,`username`= ?, `clave`= ?, `telefono` = ? WHERE id = ? ', [ns.nombre_apellido, ns.fecha_nacimiento, ns.direccion, ns.username, ns.clave, ns.telefono , id] , function (error, results, fields) {
                 if (error) throw error;
 
