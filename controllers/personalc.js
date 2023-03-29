3532f3024dbd36db6dfb80556b006f79acfe416c
OchoittaDev <87886442+OchoittaDev@users.noreply.github.com>
2023-03-29 09:44:16 -0400

    Add files via upload

---
+++ controllers/personalc.js
@@ -0,0 +1,46 @@
+const personalmodel = require('../Models/personalm');
+
+class personalcontroller {
+    Listar(){
+        return new Promise ( (resolve, reject) =>{
+            personalmodel.Listar()
+            .then((res)=>{
+                resolve(res)
+            })
+        });
+    }
+    Buscar(id){
+        return new Promise ((resolve, reject)=>{
+            personalmodel.Buscar(id)
+            .then((res)=>{
+                resolve(res)
+            });
+        })
+    }
+    Agregar(ns){
+        return new Promise ((resolve, reject)=>{
+            personalmodel.Agregar(ns)
+            .then((res)=>{
+                resolve(res)
+            });
+        })
+    }
+    Actualizar(id, ns){
+        return new Promise ((resolve, reject)=>{
+            personalmodel.Actualizar(id, ns)
+            .then((respt)=>{
+                resolve(respt)
+            })
+        })    
+    }
+    Eliminar(id){
+        return new Promise((resolve,reject)=>{
+            personalmodel.Eliminar(id)
+            .then((respt)=>{
+                resolve(respt);
+            })
+        })
+    }
+}
+
+module.exports = new personalcontroller;
---
+++ controllers/solicitantec.js
@@ -0,0 +1,46 @@
+const solicitantemodel = require('../Models/solicitantem');
+
+class solicitantecontroller {
+    Listar(){
+        return new Promise ( (resolve, reject) =>{
+            solicitantemodel.Listar()
+            .then((res)=>{
+                resolve(res)
+            })
+        });
+    }
+    Buscar(id){
+        return new Promise ((resolve, reject)=>{
+            solicitantemodel.Buscar(id)
+            .then((res)=>{
+                resolve(res)
+            });
+        })
+    }
+    Agregar(ns){
+        return new Promise ((resolve, reject)=>{
+            solicitantemodel.Agregar(ns)
+            .then((res)=>{
+                resolve(res)
+            });
+        })
+    }
+    Actualizar(id, ns){
+        return new Promise ((resolve, reject)=>{
+            solicitantemodel.Actualizar(id, ns)
+            .then((respt)=>{
+                resolve(respt)
+            })
+        })    
+    }
+    Eliminar(id){
+        return new Promise((resolve,reject)=>{
+            solicitantemodel.Eliminar(id)
+            .then((respt)=>{
+                resolve(respt);
+            })
+        })
+    }
+}
+
+module.exports = new solicitantecontroller;
